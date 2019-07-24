3f39f70efcf2782cc1a83b5b6e458d287441bb03
myname21-meet <myname21@meet.mit.edu>
2019-07-24 07:15:44 -0400

    sejo

--- style/style.js
+++ style/style.js
@@ -2,17 +2,21 @@
 
 
 
+
 function updateSize(newSize) {
+	$("img").css("width", newSize )
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
 }
 
 
 function updateLeftPosition(newX) {
 	// FINISH ME
+	$("img").css("transform", "rotateX("+ newX+ "deg)");
 
 }
 
 function updateTopPosition(newY) {
 	// FINISH ME
+	$("img").css("transform", "rotatey("+ newY+ "deg)");
 }
 
